19d21c96203f582d367d9bc0416cdea843e6d876
Marco Squillacioti <marcoenzo0501@gmail.com>
2021-06-10 12:49:10 +0200

    new file:   __init__.py 	modified:   src/data/make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -19,6 +19,6 @@ if __name__ == '__main__':
 
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+    #load_dotenv(find_dotenv())
     print(PROJECT_PATH)
     #main()
