4c69a5cf4085a4e9686f6344dfb9b9d00135e24c
Marco Squillacioti <marcoenzo0501@gmail.com>
2021-06-10 16:41:44 +0200

    modified:   src/data/make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,24 +1,24 @@
-# -*- coding: utf-8 -*-
-
-import logging
 from variables import PROJECT_PATH
-from dotenv import find_dotenv, load_dotenv
+import datasets
+from transformers import AutoTokenizer
 
+def download_data():
+    data = datasets.load_dataset('wikipedia', '20200501.en')
+    data.save_to_disk(PROJECT_PATH / 'data' / 'raw')
+    return data
 
-def main():
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+def load_data():
+    data = datasets.load_from_disk(PROJECT_PATH / 'data' / 'raw' / 'train')
+    return data
 
+def tokenize_function(datapoint):
+    return tokenizer(datapoint["text"], padding="max_length", truncation=True)
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    #load_dotenv(find_dotenv())
-    print(PROJECT_PATH)
-    #main()
+    try:
+        data = load_data()
+    except:
+        data = download_data()
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+    processed_data = data.map(tokenize_function, batched=True)
+    processed_data.save_to_disk(PROJECT_PATH / 'data' / 'processed')
