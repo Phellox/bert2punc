6439de186e977deb39c7d53a05bc6aa795e3846e
Marco Squillacioti <marcoenzo0501@gmail.com>
2021-06-17 11:13:37 +0200

    modified:   tests/test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,16 +1,16 @@
 import torch
 from torch.utils.data import TensorDataset
-from absl import flags
-import pytest
 from variables import PROJECT_PATH
 from src.models import train_model
-
 
 def load_data(path):
         X, y = torch.load(path) 
         return TensorDataset(X, y)
 
-def test_model():
-    train_model.EvalModel()
-
-print(train_model.TrainModel().model)
+def test_model(i=5):
+    model = train_model.TrainModel().model
+    data = load_data(path=PROJECT_PATH / "data" / "processed" / "val.pt")
+    with torch.no_grad():
+        out = model(data.tensors[0][:i])
+        y_pred = torch.argmax(out, dim=-1)
+    assert y_pred.shape == data.tensors[1][:i].shape
