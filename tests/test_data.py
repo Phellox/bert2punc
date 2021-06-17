f5303712a346a327abf8ba9cebe4d46ca2a26123
Marco Squillacioti <marcoenzo0501@gmail.com>
2021-06-17 11:49:57 +0200

    modified:   src/data/make_dataset.py 	modified:   tests/test_data.py 	modified:   tests/test_model.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,14 +1,12 @@
+from src.data.make_dataset import insert_target
 import torch
-from torch.utils.data import TensorDataset
 import pytest
 from variables import PROJECT_PATH
 
-def load_data(path):
-        X, y = torch.load(path) 
-        return TensorDataset(X, y)
-
 @pytest.mark.parametrize("data_set", ["train.pt", "val.pt", "test.pt"])
-def test_shapes(data_set):
-    data = load_data(path=PROJECT_PATH / "data" / "processed" / data_set)
-    assert data.tensors[0].shape[1] == 32
-    assert data.tensors[0].shape[0] == data.tensors[1].shape[0]
+def test_datasets(data_set):
+    X, y = torch.load(str(PROJECT_PATH / "data" / "processed" / data_set))
+    assert X.shape[1] == 32
+    assert X.shape[0] == y.shape[0]
+    assert isinstance(X, torch.Tensor)
+    assert isinstance(y, torch.Tensor)
--- tests/test_model.py
+++ tests/test_model.py
@@ -2,15 +2,13 @@ import torch
 from torch.utils.data import TensorDataset
 from variables import PROJECT_PATH
 from src.models import train_model
+from src.data.load_data import load_dataset
 
-def load_data(path):
-        X, y = torch.load(path) 
-        return TensorDataset(X, y)
-
-def test_model(i=5):
+def test_model_output(i=5):
     model = train_model.TrainModel().model
-    data = load_data(path=PROJECT_PATH / "data" / "processed" / "val.pt")
+    data = load_dataset(set_type = 'val', dir_path=PROJECT_PATH / "data" / "processed")
     with torch.no_grad():
         out = model(data.tensors[0][:i])
         y_pred = torch.argmax(out, dim=-1)
     assert y_pred.shape == data.tensors[1][:i].shape
+    assert type(y_pred) == type(data.tensors[1])
